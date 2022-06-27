8f5fc9e8f89ed369afb0ce34d5d45572164e7f2f
xuguang <641806618.qq.com>
2022-06-27 17:46:42 +0800

    feature:集合

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,46 @@
-use yew::prelude::*;
-
-enum Msg {
-    AddOne,
-}
-
-struct Model {
-    link: ComponentLink<Self>,
-    value: i64,
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            value: 0,
-        }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-                <p>{"pc"}</p>
-            </div>
-        }
-    }
-}
-
+use std::collections::HashMap;
+// String是对Vec<u8>的包装
+/*
+push_str: 添加字符串
+push:添加单个字符
++：字符串拼接
+*/
 fn main() {
-    yew::start_app::<Model>();
+    panic!("crash and burn");
+    let mut v = vec![1,2,3];
+    for i in &mut v {
+        *i += 50;
+        println!("{:?}", i);
+    };
+    let He = String::from("qeq");
+    let name = String::from("xuguang");
+    let s = format!("{}-{}", He, name);
+    println!("{}", s);
+    let age = String::from("h").len();
+    println!("{}", age); 
+    // let row = vec![
+    //     SpreadssheetCell::Int(3),
+    //     SpreadssheetCell::Text(String::from("blue")),
+    //     SpreadssheetCell::Float(10.12),
+    // ];
+    let mut scores: HashMap<String, i32> = HashMap::new();
+    scores.insert(String::from("Bulue"), 10); 
+    hash_map(); 
+}
+
+fn hash_map() {
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:#?}", map);
+}
+
+enum SpreadssheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
 }
